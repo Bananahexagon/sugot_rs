d536603cf073e896fd71e4256fd6a68d951f8dd0
bananahexagon <obsidian.bananafool@gmail.com>
2023-10-03 21:57:30 +0900

    #

--- src/parser/main.rs
+++ src/parser/main.rs
@@ -12,7 +12,7 @@ pub fn main(tokens: Vec<Token>) -> Vec<FuncDeclar> {
         } else if matches!(&token.val[..], ")" | "}") {
             stage -= 1;
         } else if &token.val == "fun" && stage == 0 {
-            result.push(func_decl_parse(decl));
+            result.push(func_decl(decl));
             decl = Vec::new();
         };
         decl.push(token);
@@ -20,16 +20,25 @@ pub fn main(tokens: Vec<Token>) -> Vec<FuncDeclar> {
     unimplemented!()
 }
 
-fn func_decl_parse(tokens: Vec<Token>) -> FuncDeclar {
+fn func_decl(tokens: Vec<Token>) -> FuncDeclar {
     let location = Location {
         start_line: tokens[0].location.start_line,
         start_column: tokens[0].location.start_column,
         end_line: tokens[tokens.len() - 1].location.end_line,
         end_column: tokens[tokens.len() - 1].location.end_column,
     };
+    let mut ptr = 0;
+    assert_eq!(tokens[ptr].val, "fun");
+    ptr += 1;
+    let name = tokens[1].val.clone();
+    ptr += 1;
+    assert_eq!(tokens[ptr].val, "(");
+    ptr += 1;
+    assert_eq!(tokens[ptr].val, ")");
+    ptr += 1;
     return FuncDeclar {
         location: location,
-        name: tokens[1].val.clone(),
+        name: name,
         input_types: vec![] , //TODO 今は入力を取らない関数だけ
         return_type: unimplemented!(), 
         define: unimplemented!(),
