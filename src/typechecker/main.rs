181d1190403ddde5fbd5fa61c18e20727b1a6661
bananahexagon <obsidian.bananafool@gmail.com>
2023-12-22 17:17:37 +0900

    #

--- src/typechecker/main.rs
+++ src/typechecker/main.rs
@@ -1,8 +1,13 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::types::ast::*;
 
 pub fn entry(asts: &[FuncDeclar]) -> Result<(), String> {
+    let applicable = {
+        let mut t: HashSet<(&'static str, &'static str)> = HashSet::new();
+        t.insert(("i32", "int"));
+        t
+    };
     let mut fn_signs = HashMap::new();
     for f in asts {
         if !fn_signs.contains_key(&f.name) {
@@ -11,8 +16,14 @@ pub fn entry(asts: &[FuncDeclar]) -> Result<(), String> {
             return Err(format!("Can't declare function twice: {:?}", f.location));
         }
     }
+    let mut var_signs = HashMap::new();
     for f in asts {
-        for _ in &f.define.contents {}
+        dfs(&f.define,&mut fn_signs,&mut var_signs)
     }
+
     Ok(())
+}
+
+fn dfs(block: &Block,fn_signs: &mut HashMap<String, Vec<FuncArgs>>,var_signs: &mut HashMap<String, Vec<DataType>>) {
+    for _ in &block.contents {}
 }
