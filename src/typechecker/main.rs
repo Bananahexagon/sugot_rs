3b3fbaee790fafaafe0918525d724c5655c16fa4
bananahexagon <obsidian.bananafool@gmail.com>
2023-12-23 17:19:21 +0900

    #

--- src/typechecker/main.rs
+++ src/typechecker/main.rs
@@ -1,13 +1,8 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use crate::types::ast::*;
 
 pub fn entry(asts: &[FuncDeclar]) -> Result<(), String> {
-    let apply_types = {
-        let mut t: HashSet<(&'static str, &'static str)> = HashSet::new();
-        t.insert(("i32", "int"));
-        t
-    };
     let mut fn_signs = HashMap::new();
     for f in asts {
         if !fn_signs.contains_key(&f.name) {
@@ -18,14 +13,7 @@ pub fn entry(asts: &[FuncDeclar]) -> Result<(), String> {
     }
     let mut var_signs = HashMap::new();
     for f in asts {
-        dfs(
-            &f.define,
-            &mut fn_signs,
-            &mut var_signs,
-            &apply_types,
-            0,
-            &f.return_type,
-        )?
+        dfs(&f.define, &mut fn_signs, &mut var_signs, 0, &f.return_type)?
     }
 
     Ok(())
@@ -35,36 +23,25 @@ fn dfs(
     block: &Block,
     fn_signs: &mut HashMap<String, Vec<FuncArgs>>,
     var_signs: &mut HashMap<String, Vec<DataType>>,
-    apply_types: &HashSet<(&'static str, &'static str)>,
     layer: u32,
     return_type: &DataType,
 ) -> Result<(), String> {
     for s in &block.contents {
         match s {
-            Statement::Block(b) => {
-                dfs(b, fn_signs, var_signs, apply_types, layer + 1, return_type)?
-            }
+            Statement::Block(b) => dfs(b, fn_signs, var_signs, layer + 1, return_type)?,
             Statement::If(i) => {
                 dfs(
                     &i.then_contents,
                     fn_signs,
                     var_signs,
-                    apply_types,
                     layer + 1,
                     return_type,
                 )?;
                 if let Some(c) = &i.else_contents {
-                    dfs(&c, fn_signs, var_signs, apply_types, layer + 1, return_type)?
+                    dfs(&c, fn_signs, var_signs, layer + 1, return_type)?
                 }
             }
-            Statement::While(w) => dfs(
-                &w.contents,
-                fn_signs,
-                var_signs,
-                apply_types,
-                layer + 1,
-                return_type,
-            )?,
+            Statement::While(w) => dfs(&w.contents, fn_signs, var_signs, layer + 1, return_type)?,
             Statement::Call(_) => todo!(),
             Statement::Return(_) => todo!(),
             Statement::VarDeclar(_) => todo!(),
@@ -72,4 +49,8 @@ fn dfs(
         }
     }
     Ok(())
+}
+
+fn _is_match(l: &str, r: &str) -> bool {
+    l == r || l == "any" || r == "allowed"
 }
