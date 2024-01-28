55c84e490510ab92628811edc504114f5892b591
bananahexagon <obsidian.bananafool@gmail.com>
2024-01-28 14:34:46 +0900

    re init

--- src/utils/general.rs
+++
@@ -1,9 +0,0 @@
-pub fn flatten_vec<T>(vv: Vec<Vec<T>>) -> Vec<T> {
-    let mut result = Vec::new();
-    for v in vv {
-        for p in v {
-            result.push(p);
-        }
-    }
-    result
-}
